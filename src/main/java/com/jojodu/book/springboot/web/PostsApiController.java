4087362f3c12885d12bae47ffca69397078553e4
lbs1206 <qudtjr1993@nate.com>
2020-11-18 23:16:37 +0900

    11-18 : 글 등록페이지

--- src/main/java/com/jojodu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojodu/book/springboot/web/PostsApiController.java
@@ -2,11 +2,14 @@ package com.jojodu.book.springboot.web;
 
 
 import com.jojodu.book.springboot.service.posts.PostsService;
+import com.jojodu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojodu.book.springboot.web.dto.PostsResponseDto;
 import com.jojodu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojodu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor // 선언된 모든 final 필드가 포함된 생성자를 생성, final이 없는 필드는 생성자에 포함되지 않음
 @RestController //Json 형태로 객체 데이터 반환 어노테이션
@@ -25,10 +28,19 @@ public class PostsApiController {
         return postsService.update(id, requestDto);
     }
 
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
+
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById (@PathVariable Long id){
         return postsService.findById(id);
     }
 
-
+    @GetMapping("/api/v1/posts/list")
+    public List<PostsListResponseDto> findAll() {
+        return postsService.findAllDesc();
+    }
 }
