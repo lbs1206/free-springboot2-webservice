5b864eff74bc96cfc2f00f33fddae028d3d81a30
lbs1206 <qudtjr1993@nate.com>
2020-11-29 19:47:55 +0900

    11-29 : 네이버 로그인 추가 및 기존테스트 시큐리팅 적용

--- src/test/java/com/jojodu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojodu/book/springboot/web/HelloControllerTest.java
@@ -1,25 +1,33 @@
 package com.jojodu.book.springboot.web;
 
+import com.jojodu.book.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+//@WebMvcTest //여러 스프링 테스트 어노테이션중 Web에 집중할 수 있는 어노테이션(JPA 기능 사용불가)
 @RunWith(SpringRunner.class)//스프링 부트와 JUnit 사이에 연결자 역할
-@WebMvcTest //여러 스프링 테스트 어노테이션중 Web에 집중할 수 있는 어노테이션(JPA 기능 사용불가)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTest {
 
     @Autowired //스프링이 관리하는 빈을 주입
     private MockMvc mvc;//웹 API 테스트 할떄 사용 , 스프링 MVC 테스트 의 시작점(GET,POST API 테스트가능)
 
+    @WithMockUser(roles="USER")
     @Test
     public void hello_return() throws Exception{
         String hello = "hello";
@@ -30,6 +38,7 @@ public class HelloControllerTest {
 
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloDto_return() throws Exception{
         String name = "hello";
